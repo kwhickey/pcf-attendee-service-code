030bda42b46ee8f309c4db276ae050aecd91a138
Keith Hickey <hickey_keith@bah.com>
2017-03-11 08:04:26 -0500

    Adding more logging to packaging script

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -2,19 +2,34 @@
 
 set -e +x
 
-pushd attendee-service-source
+sourcedir=attendee-service-source
+outputdir=package-output
+jarname=attendee-service.jar
+
+echo "VARIABLES: \
+  sourcedir=$sourcedir \
+  outputdir=$outputdir \
+  jarname=$jarname"
+
+pushd $sourcedir
   echo "Packaging JAR"
   ./mvnw clean package -DskipTests
 popd
 
-jar_count=`find attendee-service-source/target -type f -name *.jar | wc -l`
+jar_count=`find $sourcedir/target -type f -name *.jar | wc -l`
 
 if [ $jar_count -gt 1 ]; then
-  echo "More than one jar found, don't know which one to deploy. Exiting"
+  echo "More than one jar found at $sourcedir/target, don't know which one to deploy. Exiting"
   exit 1
 fi
 
-find attendee-service-source/target -type f -name *.jar -exec cp "{}" package-output/attendee-service.jar \;
+if [ $jar_count -eq 0 ]; then
+  echo "No jar found at $sourcedir/target. Ensure it was built. Exiting"
+  exit 1
+fi
+
+echo "Copying jar from $sourcedir/target to $outputdir/$jarname"
+find $sourcedir/target -type f -name *.jar -exec cp "{}" $outputdir/$jarname \;
 
 echo "Done packaging"
 exit 0
