506185d265e49655fda775e5b9f5591b5ce854ab
Keith Hickey <hickey_keith@bah.com>
2017-03-11 08:13:50 -0500

    Adding checks for output dir existence in package

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -6,9 +6,9 @@ sourcedir=attendee-service-source
 outputdir=package-output
 jarname=attendee-service.jar
 
-echo "VARIABLES: \
-  sourcedir=$sourcedir \
-  outputdir=$outputdir \
+echo "VARIABLES: 
+  sourcedir=$sourcedir 
+  outputdir=$outputdir
   jarname=$jarname"
 
 pushd $sourcedir
@@ -17,6 +17,8 @@ pushd $sourcedir
 popd
 
 jar_count=`find $sourcedir/target -type f -name *.jar | wc -l`
+
+echo "Found $jar_count JAR file(s) at $sourcedir/target"
 
 if [ $jar_count -gt 1 ]; then
   echo "More than one jar found at $sourcedir/target, don't know which one to deploy. Exiting"
@@ -28,7 +30,14 @@ if [ $jar_count -eq 0 ]; then
   exit 1
 fi
 
-echo "Copying jar from $sourcedir/target to $outputdir/$jarname"
+echo "Checking working directory ($(pwd)) contents..."
+ls -la
+
+echo "Copying jar from $sourcedir/target to $outputdir/$jarname, from working directory: $(pwd)"
+if [ ! -d "$outputdir" ]; then
+  echo "Directory $outputdir does not exist. Creating it."
+  mkdir $outputdir
+fi
 find $sourcedir/target -type f -name *.jar -exec cp "{}" $outputdir/$jarname \;
 
 echo "Done packaging"
