5ff78c95baed35200b788c9d2cc45cbcd824673a
Keith Hickey <hickey_keith@bah.com>
2017-03-13 10:46:05 -0600

    Editing smoke-test.sh to be responsive to with and without hostname suffix

--- ci/tasks/smoke-test.sh
+++ ci/tasks/smoke-test.sh
@@ -12,12 +12,18 @@ if [ -z $ATTENDEE_SERVICE_HOSTNAME ]; then
   exit 1
 fi
 
+if [ -z $ATTENDEE_SERVICE_HOSTNAME_SUFFIX ]; then
+  echo "Using a suffix on the hostname: $ATTENDEE_SERVICE_HOSTNAME_SUFFIX"
+else
+  echo "No suffix supplied to hostname. Using standard hostname."
+fi
+
 echo "... updating apt-get and ensuring curl"
 apt-get update && apt-get install -y curl
 
 pushd attendee-service-source
   echo "Running smoke tests for Attendee Service deployed at $ATTENDEE_SERVICE_URL"
-  smoke-tests/bin/test "http://$ATTENDEE_SERVICE_HOSTNAME-bg-rc.$ATTENDEE_SERVICE_DOMAIN"
+  smoke-tests/bin/test "http://$ATTENDEE_SERVICE_HOSTNAME$ATTENDEE_SERVICE_HOSTNAME_SUFFIX.$ATTENDEE_SERVICE_DOMAIN"
 popd
 
 exit 0
