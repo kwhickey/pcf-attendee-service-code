3ef66e7a57e84a4f73706504cd66dc42f12b39df
Keith Hickey <hickey_keith@bah.com>
2017-03-13 11:18:57 -0600

    Trying better string var concatenation

--- ci/tasks/smoke-test.sh
+++ ci/tasks/smoke-test.sh
@@ -13,17 +13,19 @@ if [ -z $ATTENDEE_SERVICE_HOSTNAME ]; then
 fi
 
 if [ -z $ATTENDEE_SERVICE_HOSTNAME_SUFFIX ]; then
-  echo "Using a suffix on the hostname: $ATTENDEE_SERVICE_HOSTNAME_SUFFIX"
-else
   echo "No suffix supplied to hostname. Using standard hostname."
+else
+  echo "Using a suffix on the hostname: $ATTENDEE_SERVICE_HOSTNAME_SUFFIX"
 fi
+
+url="http://${ATTENDEE_SERVICE_HOSTNAME}${ATTENDEE_SERVICE_HOSTNAME_SUFFIX}.${ATTENDEE_SERVICE_DOMAIN}"
 
 echo "... updating apt-get and ensuring curl"
 apt-get update && apt-get install -y curl
 
 pushd attendee-service-source
-  echo "Running smoke tests for Attendee Service deployed at $ATTENDEE_SERVICE_URL"
-  smoke-tests/bin/test "http://$ATTENDEE_SERVICE_HOSTNAME$ATTENDEE_SERVICE_HOSTNAME_SUFFIX.$ATTENDEE_SERVICE_DOMAIN"
+  echo "Running smoke tests for Attendee Service deployed at: $url"
+  smoke-tests/bin/test $url
 popd
 
 exit 0
